89bafc1716c17ae0f06b1366a3cfc56cef16ad45
imlxtif <abdelatifguendouz72@gmail.com>
2023-06-06 12:24:54 +0100

    hh

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/revideox/"
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt', 'coi-serviceworker.js'],
+      manifest: {
+        name: 'RevideoX',
+        short_name: 'RevideoX',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/revideox/vite.svg',
+            sizes: '128x128',
+            type: 'image/svg+xml',
+          },
+        ],
+      },
+    }),
+  ],
+  base: "/revideox/",
 })
