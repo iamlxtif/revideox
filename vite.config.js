a350af784a43ee12e303eb6a9c0883f377610d87
imlxtif <abdelatifguendouz72@gmail.com>
2023-06-06 09:41:19 +0100

    new

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/ffmpegapp/",
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'src/main.jsx',
-        'coi-serviceworker': '/coi-serviceworker.js',
-      },
-    },
-  },
 })
