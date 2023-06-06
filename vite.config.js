c311931d6f3a3eca8ee0057977df39870cde1635
imlxtif <abdelatifguendouz72@gmail.com>
2023-06-06 11:00:12 +0100

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/revideox/",
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        main: '/index.html',
-      },
-    },
-  },
+  base: "/revideox/"
 })
