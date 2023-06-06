f48f83f62257881c63877be05944a60ca29a00a6
imlxtif <abdelatifguendouz72@gmail.com>
2023-06-06 12:53:28 +0100

    hh

--- vite.config.js
+++ vite.config.js
@@ -7,16 +7,4 @@ export default defineConfig({
     react(),
   ],
   base: "/revideox/",
-  build: {
-    rollupOptions: {
-      input: {
-        main: '/index.html',
-        serviceworker: 'coi-serviceworker.js',
-      },
-      output: {
-        entryFileNames: 'assets/[name]-[hash].js',
-      },
-    },
-  },
-  assetsInclude: ['coi-serviceworker.js'],
 })
