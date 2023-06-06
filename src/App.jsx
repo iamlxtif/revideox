c747524585002aa99a8dd941056c57bdc94774ca
imlxtif <abdelatifguendouz72@gmail.com>
2023-06-06 14:11:15 +0100

    hhh

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ function App() {
           <Router>
             <Navbar />
             <Routes>
-              <Route path="/" exact element={<Home/>} />
+              <Route path="" exact element={<Home/>} />
               <Route path="/about" element={<About/>} />
               <Route path="/contact" element={<Contact/>} />
               <Route path="/Tools" element={<Tools/>} />
